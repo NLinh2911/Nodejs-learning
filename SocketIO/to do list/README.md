e4f39c733e6653333c8d89b2b41d98b1212f5cef
NLinh2911 <nlinh2911@gmail.com>
2017-03-28 22:55:16 +0700

    add simple mongoDB with mLab

---
+++ SocketIO/to do list/README.md
@@ -0,0 +1,12 @@
+# REAL TIME CONNECTED TODO LIST APP WITH SOCKET.IO AND MONGO DB
+
+### TODO LIST CORE FUNCTIONALITIES:
+1. Enter an username on alert box
+2. Add a new todo
+3. Strike through a finished todo
+4. Delete completely a todo
+
+### USE SOCKET.IO TO SHARE TODO LIST BETWEEN MULTIPLE CONNECTED USERS
+Add, finish or delete a todo and all other connected users will be alerted and their todo list also get updated
+
+### USE MONGO DB AND MLAB TO STORE A SIMPLE DB
--- SocketIO/to do list/index.js
+++ SocketIO/to do list/index.js
@@ -4,42 +4,67 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const ent = require('ent');
-const fs = require('fs');
+const shortid = require('shortid');
+const mongoose = require('mongoose');
 
+// static files
 const assetsPath = path.resolve(__dirname, "assets");
 app.use('/assets', express.static(assetsPath));
 
-// Load index.html
+// Connect to database
+mongoose.connect('mongodb://test:test@ds143900.mlab.com:43900/todosocket');
+// Create a scheme - blueprint for data
+const todoSchema = new mongoose.Schema({
+  todo: String,
+  id: String
+})
+// 
+const Todo = mongoose.model('Todo', todoSchema);
+
+// Routing
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   // when an user connects, their username is received and stored as a session var
+
   socket.on('new_client', (username) => {
-      username = ent.encode(username);
-      socket.username = username;
-      socket.broadcast.emit('new_client', username);
-      console.log( socket.username + ' connected');
+    username = ent.encode(username);
+    socket.username = username;
+    socket.broadcast.emit('new_client', username);
+    console.log(socket.username + ' connected');
   });
+    // get all todo from db and send to user on connection
+  Todo.find({}, function (err, data){
+    if (err) throw err;
+    socket.emit('update data', data);
+    console.log(data);
+  })
   // disconnet
   socket.on('disconnect', () => {
     console.log(socket.username + ' disconnected');
   });
   // server receives data when a client adds a new to-do
-  socket.on('new_todo', (data) => {
-    //send to other clients with username
-    data.username = socket.username;
-    socket.broadcast.emit('new_todo', data);
-    console.log(data);
+  socket.on('new_todo', function (todoText) {
+    let todoId = shortid.generate();
+    // store new todo in the db
+    let newTodo = Todo({todo: todoText, id: todoId}).save(function (err, data) {
+      if (err) throw err;
+    });
+
+    io.sockets.emit('new_todo', { username: socket.username, id: todoId, text: todoText });
   });
   // server receives data when a client finishes a to-do
   socket.on('done_todo', (doneId) => {
-    socket.broadcast.emit('done_todo', {username: socket.username, id: doneId});
+    socket.broadcast.emit('done_todo', { username: socket.username, id: doneId });
   });
   // server receives data when a client deletes a to-do
   socket.on('delete_todo', (deletedId) => {
-    socket.broadcast.emit('delete_todo', {username: socket.username, id: deletedId});
+    Todo.find({id: deletedId}).remove(function (err, data) {
+      if (err) throw err;
+    })
+    socket.broadcast.emit('delete_todo', { username: socket.username, id: deletedId });
   });
 
 });
